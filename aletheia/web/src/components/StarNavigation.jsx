27f981c133e91fe16fefebba7d0f3764ab3f3a5c
huangfuyaoming <w9687777@163.com>
2026-09-13 18:43:07 +0800

    Delete aletheia directory

--- aletheia/web/src/components/StarNavigation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import StarButton from './StarTransition.jsx';
-
-export default function StarNavigation({ onChat, onHistory }) {
-  return <nav className="star-navigation" aria-label="星空导航">
-    <StarButton className="conversation-star" onClick={onChat} label="对话之星，打开全屏对话" name="对话" />
-    <StarButton className="memory-star" onClick={onHistory} label="记忆之星，打开全屏历史" name="历史" orbit />
-  </nav>;
-}
--- aletheia/web/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: { proxy: { '/api': 'http://127.0.0.1:6006' } },
-  build: { target: 'es2022', sourcemap: false },
-});
