cc7b85d1247d4aa3a68cef94b94d7437de663cdf
huangfuyaoming <w9687777@163.com>
2026-09-13 18:23:30 +0800

    Delete aletheia directory

--- aletheia/cleanup.py
+++
@@ -1,8 +0,0 @@
-from server.db import Database
-from server.maintenance import cleanup_expired
-from server.settings import settings
-
-if __name__ == '__main__':
-    config = settings()
-    count = cleanup_expired(Database(config['DATABASE']), config['DATA_DIR'])
-    print(f'Removed {count} expired finished tasks.')
--- aletheia/web/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { StarTransitionProvider } from './components/StarTransition.jsx';
-import './style.css';
-import './constellation.css';
-
-createRoot(document.getElementById('root')).render(<React.StrictMode><StarTransitionProvider><App /></StarTransitionProvider></React.StrictMode>);
